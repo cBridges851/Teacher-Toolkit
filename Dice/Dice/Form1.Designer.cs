b28d6ccc3cb44ef9a91c7e6b915c879a1c2ce3b5
Mr-No-One <59934038+Mr-No-One@users.noreply.github.com>
2020-01-26 21:51:38 +0000

    Add files via upload

--- Dice/Dice/Form1.Designer.cs
+++ Dice/Dice/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Dice
 {
-    partial class Form1
+    partial class Dice
     {
         /// <summary>
         /// Required designer variable.
@@ -31,13 +31,25 @@
             this.buttonStart = new System.Windows.Forms.Button();
             this.textBoxDiceOne = new System.Windows.Forms.TextBox();
             this.textBoxDiceTwo = new System.Windows.Forms.TextBox();
+            this.pictureBoxDiceOne = new System.Windows.Forms.PictureBox();
+            this.pictureBoxDiceTwo = new System.Windows.Forms.PictureBox();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.pictureBoxDiceThree = new System.Windows.Forms.PictureBox();
+            this.textBoxDiceThree = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButtonDiceOne = new System.Windows.Forms.RadioButton();
+            this.radioButtonDiceTwo = new System.Windows.Forms.RadioButton();
+            this.radioButtonDiceThree = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDiceOne)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDiceTwo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDiceThree)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(235, 219);
+            this.buttonStart.Location = new System.Drawing.Point(356, 461);
             this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.Size = new System.Drawing.Size(121, 33);
             this.buttonStart.TabIndex = 0;
             this.buttonStart.Text = "Start";
             this.buttonStart.UseVisualStyleBackColor = true;
@@ -45,28 +57,129 @@
             // 
             // textBoxDiceOne
             // 
-            this.textBoxDiceOne.Location = new System.Drawing.Point(125, 104);
+            this.textBoxDiceOne.Location = new System.Drawing.Point(91, 383);
             this.textBoxDiceOne.Name = "textBoxDiceOne";
             this.textBoxDiceOne.Size = new System.Drawing.Size(97, 20);
             this.textBoxDiceOne.TabIndex = 1;
             // 
             // textBoxDiceTwo
             // 
-            this.textBoxDiceTwo.Location = new System.Drawing.Point(314, 103);
+            this.textBoxDiceTwo.Location = new System.Drawing.Point(365, 383);
             this.textBoxDiceTwo.Name = "textBoxDiceTwo";
             this.textBoxDiceTwo.Size = new System.Drawing.Size(100, 20);
             this.textBoxDiceTwo.TabIndex = 2;
             // 
-            // Form1
+            // pictureBoxDiceOne
+            // 
+            this.pictureBoxDiceOne.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxDiceOne.Location = new System.Drawing.Point(27, 107);
+            this.pictureBoxDiceOne.Name = "pictureBoxDiceOne";
+            this.pictureBoxDiceOne.Size = new System.Drawing.Size(256, 256);
+            this.pictureBoxDiceOne.TabIndex = 3;
+            this.pictureBoxDiceOne.TabStop = false;
+            // 
+            // pictureBoxDiceTwo
+            // 
+            this.pictureBoxDiceTwo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxDiceTwo.Location = new System.Drawing.Point(289, 107);
+            this.pictureBoxDiceTwo.Name = "pictureBoxDiceTwo";
+            this.pictureBoxDiceTwo.Size = new System.Drawing.Size(256, 256);
+            this.pictureBoxDiceTwo.TabIndex = 4;
+            this.pictureBoxDiceTwo.TabStop = false;
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(861, 22);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(75, 23);
+            this.buttonBack.TabIndex = 5;
+            this.buttonBack.Text = "Back";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxDiceThree
+            // 
+            this.pictureBoxDiceThree.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxDiceThree.Location = new System.Drawing.Point(551, 107);
+            this.pictureBoxDiceThree.Name = "pictureBoxDiceThree";
+            this.pictureBoxDiceThree.Size = new System.Drawing.Size(256, 256);
+            this.pictureBoxDiceThree.TabIndex = 6;
+            this.pictureBoxDiceThree.TabStop = false;
+            // 
+            // textBoxDiceThree
+            // 
+            this.textBoxDiceThree.Location = new System.Drawing.Point(633, 383);
+            this.textBoxDiceThree.Name = "textBoxDiceThree";
+            this.textBoxDiceThree.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDiceThree.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Number of dice";
+            // 
+            // radioButtonDiceOne
+            // 
+            this.radioButtonDiceOne.AutoSize = true;
+            this.radioButtonDiceOne.Location = new System.Drawing.Point(157, 57);
+            this.radioButtonDiceOne.Name = "radioButtonDiceOne";
+            this.radioButtonDiceOne.Size = new System.Drawing.Size(31, 17);
+            this.radioButtonDiceOne.TabIndex = 9;
+            this.radioButtonDiceOne.TabStop = true;
+            this.radioButtonDiceOne.Text = "1";
+            this.radioButtonDiceOne.UseVisualStyleBackColor = true;
+            this.radioButtonDiceOne.CheckedChanged += new System.EventHandler(this.radioButtonDiceOne_CheckedChanged);
+            // 
+            // radioButtonDiceTwo
+            // 
+            this.radioButtonDiceTwo.AutoSize = true;
+            this.radioButtonDiceTwo.Location = new System.Drawing.Point(212, 57);
+            this.radioButtonDiceTwo.Name = "radioButtonDiceTwo";
+            this.radioButtonDiceTwo.Size = new System.Drawing.Size(31, 17);
+            this.radioButtonDiceTwo.TabIndex = 10;
+            this.radioButtonDiceTwo.TabStop = true;
+            this.radioButtonDiceTwo.Text = "2";
+            this.radioButtonDiceTwo.UseVisualStyleBackColor = true;
+            this.radioButtonDiceTwo.CheckedChanged += new System.EventHandler(this.radioButtonDiceTwo_CheckedChanged);
+            // 
+            // radioButtonDiceThree
+            // 
+            this.radioButtonDiceThree.AutoSize = true;
+            this.radioButtonDiceThree.Location = new System.Drawing.Point(269, 57);
+            this.radioButtonDiceThree.Name = "radioButtonDiceThree";
+            this.radioButtonDiceThree.Size = new System.Drawing.Size(31, 17);
+            this.radioButtonDiceThree.TabIndex = 11;
+            this.radioButtonDiceThree.TabStop = true;
+            this.radioButtonDiceThree.Text = "3";
+            this.radioButtonDiceThree.UseVisualStyleBackColor = true;
+            this.radioButtonDiceThree.CheckedChanged += new System.EventHandler(this.radioButtonDiceThree_CheckedChanged);
+            // 
+            // Dice
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(592, 435);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(953, 620);
+            this.Controls.Add(this.radioButtonDiceThree);
+            this.Controls.Add(this.radioButtonDiceTwo);
+            this.Controls.Add(this.radioButtonDiceOne);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxDiceThree);
+            this.Controls.Add(this.pictureBoxDiceThree);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.pictureBoxDiceTwo);
+            this.Controls.Add(this.pictureBoxDiceOne);
             this.Controls.Add(this.textBoxDiceTwo);
             this.Controls.Add(this.textBoxDiceOne);
             this.Controls.Add(this.buttonStart);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "Dice";
+            this.Text = "Dice";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDiceOne)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDiceTwo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDiceThree)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,6 +190,15 @@
         private System.Windows.Forms.Button buttonStart;
         private System.Windows.Forms.TextBox textBoxDiceOne;
         private System.Windows.Forms.TextBox textBoxDiceTwo;
+        private System.Windows.Forms.PictureBox pictureBoxDiceOne;
+        private System.Windows.Forms.PictureBox pictureBoxDiceTwo;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.PictureBox pictureBoxDiceThree;
+        private System.Windows.Forms.TextBox textBoxDiceThree;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButtonDiceOne;
+        private System.Windows.Forms.RadioButton radioButtonDiceTwo;
+        private System.Windows.Forms.RadioButton radioButtonDiceThree;
     }
 }
 
--- Dice/Dice/Form1.cs
+++ Dice/Dice/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Dice
 {
-    public partial class Form1 : Form
+    public partial class Dice : Form
     {
-        public Form1()
+        public Dice()
         {
             InitializeComponent();
         }
@@ -21,11 +21,147 @@ namespace Dice
         {
             Random rnd = new Random();
 
-            int rndDiceOne = rnd.Next(1, 8);
-            int rndDiceTwo = rnd.Next(1, 8);
+            int rndDiceOne = rnd.Next(1, 7);
+            int rndDiceTwo = rnd.Next(1, 7);
+            int rndDiceThree = rnd.Next(1, 7);
 
+            //KJL 2020-01-26 18:51
+            //Check dice one for its number and load corresponding image
+            if (rndDiceOne == 1)
+            {
+                pictureBoxDiceOne.Image = global::Dice.Properties.Resources.Dice1;
+            }
+
+            if (rndDiceOne == 2)
+            {
+                pictureBoxDiceOne.Image = global::Dice.Properties.Resources.Dice2;
+            }
+
+            if (rndDiceOne == 3)
+            {
+                pictureBoxDiceOne.Image = global::Dice.Properties.Resources.Dice3;
+            }
+
+            if (rndDiceOne == 4)
+            {
+                pictureBoxDiceOne.Image = global::Dice.Properties.Resources.Dice4;
+            }
+
+            if (rndDiceOne == 5)
+            {
+                pictureBoxDiceOne.Image = global::Dice.Properties.Resources.Dice5;
+            }
+
+            if (rndDiceOne == 6)
+            {
+                pictureBoxDiceOne.Image = global::Dice.Properties.Resources.Dice6;
+            }
+
+            //KJL 2020-01-26 18:58
+            //Check dice two for its number and load corresponding image
+            if (rndDiceTwo == 1)
+            {
+                pictureBoxDiceTwo.Image = global::Dice.Properties.Resources.Dice1;
+            }
+
+            if (rndDiceTwo == 2)
+            {
+                pictureBoxDiceTwo.Image = global::Dice.Properties.Resources.Dice2;
+            }
+
+            if (rndDiceTwo == 3)
+            {
+                pictureBoxDiceTwo.Image = global::Dice.Properties.Resources.Dice3;
+            }
+
+            if (rndDiceTwo == 4)
+            {
+                pictureBoxDiceTwo.Image = global::Dice.Properties.Resources.Dice4;
+            }
+
+            if (rndDiceTwo == 5)
+            {
+                pictureBoxDiceTwo.Image = global::Dice.Properties.Resources.Dice5;
+            }
+
+            if (rndDiceTwo == 6)
+            {
+                pictureBoxDiceTwo.Image = global::Dice.Properties.Resources.Dice6;
+            }
+
+            //KJL 2020-01-26 21:23
+            //Check dice two for its number and load corresponding image
+            if (rndDiceThree == 1)
+            {
+                pictureBoxDiceThree.Image = global::Dice.Properties.Resources.Dice1;
+            }
+
+            if (rndDiceThree == 2)
+            {
+                pictureBoxDiceThree.Image = global::Dice.Properties.Resources.Dice2;
+            }
+
+            if (rndDiceThree == 3)
+            {
+                pictureBoxDiceThree.Image = global::Dice.Properties.Resources.Dice3;
+            }
+
+            if (rndDiceThree == 4)
+            {
+                pictureBoxDiceThree.Image = global::Dice.Properties.Resources.Dice4;
+            }
+
+            if (rndDiceThree == 5)
+            {
+                pictureBoxDiceThree.Image = global::Dice.Properties.Resources.Dice5;
+            }
+
+            if (rndDiceThree == 6)
+            {
+                pictureBoxDiceThree.Image = global::Dice.Properties.Resources.Dice6;
+            }
+
+            //KJL 2020-01-26 21:21
+            //Display value of dice in textbox to test if it matches dice image
             textBoxDiceOne.Text = Convert.ToString(rndDiceOne);
             textBoxDiceTwo.Text = Convert.ToString(rndDiceTwo);
+            textBoxDiceThree.Text = Convert.ToString(rndDiceThree);
+        }
+
+        //KJL 2020-01-26 21:37
+        //If number of dice selected is 1, Dice 2 and 3 becomes invisible and 1 becomes visible
+        private void radioButtonDiceOne_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonDiceOne.Checked == true)
+            {
+                pictureBoxDiceOne.Visible = true;
+                pictureBoxDiceTwo.Visible = false;
+                pictureBoxDiceThree.Visible = false;
+            }
+        }
+
+        //KJL 2020-01-26 21:42
+        //If number of dice selected is 2, Dice 3 becomes invisible and 1 and 2 becomes visible
+        private void radioButtonDiceTwo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonDiceTwo.Checked == true)
+            {
+                pictureBoxDiceOne.Visible = true;
+                pictureBoxDiceTwo.Visible = true;
+                pictureBoxDiceThree.Visible = false;
+            }
+        }
+
+        //KJL 2020-01-26 21:42
+        //If number of dice selected is 3, all dice become visible
+        private void radioButtonDiceThree_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonDiceThree.Checked == true)
+            {
+                pictureBoxDiceOne.Visible = true;
+                pictureBoxDiceTwo.Visible = true;
+                pictureBoxDiceThree.Visible = true;
+            }
         }
     }
 }
