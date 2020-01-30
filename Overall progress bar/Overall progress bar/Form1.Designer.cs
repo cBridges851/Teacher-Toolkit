f34804f8b8bcc7c8bc671cf549cd5822cafd3c98
cBridges851 <54030147+cBridges851@users.noreply.github.com>
2020-01-30 10:05:32 +0000

    Add files via upload
    Interface has been started for the group progress bar

---
+++ Overall progress bar/Overall progress bar/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Overall_progress_bar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.groupBoxNovice = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBoxNovice.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 167);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "I feel that I am doing an okay job";
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPlus.Location = new System.Drawing.Point(6, 247);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(59, 39);
+            this.buttonPlus.TabIndex = 2;
+            this.buttonPlus.Text = "+ 1";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMinus.Location = new System.Drawing.Point(86, 247);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(59, 39);
+            this.buttonMinus.TabIndex = 3;
+            this.buttonMinus.Text = " - 1";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxNovice
+            // 
+            this.groupBoxNovice.Controls.Add(this.buttonPlus);
+            this.groupBoxNovice.Controls.Add(this.buttonMinus);
+            this.groupBoxNovice.Controls.Add(this.label1);
+            this.groupBoxNovice.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxNovice.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxNovice.Name = "groupBoxNovice";
+            this.groupBoxNovice.Size = new System.Drawing.Size(151, 318);
+            this.groupBoxNovice.TabIndex = 4;
+            this.groupBoxNovice.TabStop = false;
+            this.groupBoxNovice.Text = "Novice";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(170, 23);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(163, 415);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBoxNovice);
+            this.Name = "Form1";
+            this.Text = "Overall progress bar";
+            this.groupBoxNovice.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.GroupBox groupBoxNovice;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
